e6d7b608923d1afb64d5cf1f9b020569d26dda47
mariam-hamada <168047165+mariam-hamada@users.noreply.github.com>
2026-09-05 05:27:12 +0300

    Add project files.

---
+++ Assignment12/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
